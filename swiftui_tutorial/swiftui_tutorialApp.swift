40728a2731aeaaf0e82a8942cce099a86a199c91
Dhruvilpatel2303 <pateldhruvil2303@gmail.com>
2025-06-26 21:03:36 +0530

    Initial Commit

---
+++ swiftui_tutorial/swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialApp.swift
+//  swiftui_tutorial
+//
+//  Created by Haresh Patel on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui_tutorialTests/swiftui_tutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialTests.swift
+//  swiftui_tutorialTests
+//
+//  Created by Haresh Patel on 6/26/25.
+//
+
+import Testing
+@testable import swiftui_tutorial
+
+struct swiftui_tutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
